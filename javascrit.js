4edc064c6b982bc48adf97c9ab394094ec820b38
Pereiraaa-Lucas <lucas.s.pereira0099@gmail.com>
2026-03-29 11:23:46 -0300

    Add event listener to toggle navigation menu

--- javascrit.js
+++ javascrit.js
@@ -72,3 +72,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+const menuIcon = document.getElementById('menu-icon');
+const navMenu = document.getElementById('nav-menu');
+
+menuIcon.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
